3f8a1d31e4073fcaa70c08d29cf4ba302d54c80f
Aditya22 <151610172@smkassalaambandung.sch.id>
2017-07-27 16:38:55 +0700

    latihan1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel</title>
+</head>
+<body>
+Aditya Dwi Setiawan
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,40 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+	return '<h1>Halo</h1>'
+	.'Selamat Datang di Webapp Saya<br>'
+	.'laravel, emang keren.';
+});
+
+Route::get('latihan',function(){
+	return view('about');
+});
+
+Route::get('/about/1',function(){
+	return view('about.satu');
+});
+
+Route::get('/about/2',function(){
+	return view('about.dua');
+});
+
+Route::get('/about/3',function(){
+	return view('about.tiga');
+});
+
+Route::get('/about/{halaman}',function($id){
+	return 'Halaman About'.$id;
+});
+
+Route::get('/profil/{nama}/{sekolah}/{umur}',function($nama,$sekolah,$umur){
+	return 'Nama Saya : '.$nama.'<br>'
+	.'Sekolah : '.$sekolah.'<br>'
+	.'Umur : '.$umur;
+});
+
+Route::get('/nama/{halaman}',function($id){
+	$id= ' John';
+	return 'Halaman About'.$id;
+});
